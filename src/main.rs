541620cb21fa95daf2fa7fde368c9265218d861f
Tim Horlacher <tim.horlacher@protonmail.com>
2022-04-24 14:56:16 +0200

    basic application
    * Added async stuff
    
    * Fixed warnings
    
    * Added ultra basic ui stuff

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,22 @@ use matrix_sdk::{
 };
 use url::Url;
 
+use tokio::sync::mpsc;
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    text::{Span, Spans},
+    widgets::{Block, Borders, List, ListItem},
+    Frame, Terminal,
+};
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -22,29 +38,132 @@ struct Args {
     password: String,
 }
 
+struct App {
+    /// History of recorded messages
+    messages: Vec<(String, String)>,
+}
+
+impl Default for App {
+    fn default() -> App {
+        App {
+            messages: Vec::new(),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let (tx, rx) = mpsc::channel(100);
+
     let args = Args::parse();
 
     let homeserverurl = Url::parse(&args.home_server)?;
+
     let client = Client::new(homeserverurl)?;
     // First we need to log in.
     client
         .login(&args.username, &args.password, None, None)
         .await?;
-    println!("Successfully logged in");
+
     client
-        .register_event_handler(|ev: SyncMessageEvent<MessageEventContent>| async move {
-            println!(
-                "Received a message {:?} from {:?}",
-                ev.content.msgtype, ev.sender
-            );
+        .register_event_handler({
+            let tx = tx.clone();
+            move |ev: SyncMessageEvent<MessageEventContent>| {
+                let tx = tx.clone();
+                async move {
+                    tx.send(ev).await.unwrap();
+                }
+            }
         })
         .await;
 
     // Syncing is important to synchronize the client state with the server.
     // This method will never return.
-    client.sync(SyncSettings::default()).await;
+    tokio::spawn(async move {
+        client.sync(SyncSettings::default()).await;
+    });
+
+    //UI Stuff
+
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let app = App::default();
+    let _res = run_app(&mut terminal, app, rx);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    // create app and run it
 
     Ok(())
+}
+
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    mut app: App,
+    mut rx: mpsc::Receiver<SyncMessageEvent<MessageEventContent>>,
+) -> io::Result<()> {
+    loop {
+        //Check rx
+        if let Some(ev) = rx.try_recv().ok() {
+            app.messages.push((
+                ev.sender.to_string(),
+                (format!("{:?}", ev.content.msgtype)).to_string(),
+            ));
+        }
+
+        terminal.draw(|f| ui(f, &app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => {
+                    return Ok(());
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+    let block = Block::default()
+        .title("Matrix-Client")
+        .borders(Borders::ALL);
+    f.render_widget(block, chunks[0]);
+
+    let messages: Vec<ListItem> = app
+        .messages
+        .iter()
+        .enumerate()
+        .map(|(_i, m)| {
+            let content = vec![Spans::from(Span::raw(format!("{}: {}", m.0, m.1)))];
+            ListItem::new(content)
+        })
+        .collect();
+    let messages =
+        List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
+    f.render_widget(messages, chunks[1]);
 }
